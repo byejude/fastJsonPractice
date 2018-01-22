318e3c09c9d2f9ac22ef090f964fe459a4680bad
byejude4RP <657401262@qq.com>
2018-01-22 12:28:36 +0800

    update

---
+++ User.java
@@ -0,0 +1,12 @@
+package com.unionman.WatchDog.utils.FastJsonTest;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private int id;
+    private String name;
+    private String add;
+    private String old;
+}
---
+++ Word.java
@@ -0,0 +1,20 @@
+package com.unionman.WatchDog.utils.FastJsonTest;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+@Data
+public class Word {
+
+    private String d;
+    private String e;
+    private String f;
+    private String a;
+    private int b;
+    private boolean c;
+    private Date date;
+    private Map<String , Object> map;
+    private List<User> list;
+}
